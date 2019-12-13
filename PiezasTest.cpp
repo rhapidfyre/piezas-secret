b41307c8d5256ad2ef98dc1bd80d9ddffe2957e7
rhapidfyre <maharris84@hotmail.com>
2019-12-13 11:05:51 -0800

    added a few more basic test cases

--- PiezasTest.cpp
+++ PiezasTest.cpp
@@ -46,13 +46,37 @@ TEST(Piezas, testObtain) {
   ASSERT_EQ(pieza.dropPiece(0), X);
 }
 
-// Ensure board can reset
-TEST(Piezas, resetBoard) {
+// Does the turn toggle properly
+TEST(Piezas, testObtain) {
+  Piezas pieza;
+  ASSERT_EQ(pieza.dropPiece(0), X);
+  ASSERT_EQ(pieza.dropPiece(0), O);
+  ASSERT_EQ(pieza.dropPiece(0), X);
+}
+
+// Ensure board can reset after a piece has been placed
+TEST(Piezas, resetBoard) { 
   
   Piezas pieza;
   pieza.dropPiece(0);
+  ASSERT_EQ(pieza.pieceAt(0,0), X);
   
   pieza.reset();
-  ASSERT_EQ(pieza.pieceAt(0,0), Blank);
+  for(int i=0; i<BOARD_ROWS; i++)
+    for(int j=0; j<BOARD_COLS; j++)
+      ASSERT_EQ(pieza.pieceAt(i,j), Blank);
     
+}
+
+// Ensure that pieces dropped are accurate
+TEST(Piezas, fullColumn) {
+  Piezas pieza;
+  pieza.dropPiece(0);
+  pieza.dropPiece(0);
+  pieza.dropPiece(0);
+  ASSERT_EQ(pieza.dropPiece(0), Invalid);
+}
+
+TEST(Piezas, ) {
+  
 }
